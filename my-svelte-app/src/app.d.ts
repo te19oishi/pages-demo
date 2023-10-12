c187cadfd4d0dbef2c1ff1a6d46083f6e844411f
te19oishi <hinaharu.0014@gmail.com>
2023-10-12 09:24:46 +0900

    setting

--- my-svelte-app/src/app.d.ts
+++ my-svelte-app/src/app.d.ts
@@ -1,12 +1,17 @@
-// See https://kit.svelte.dev/docs/types#app
-// for information about these interfaces
-declare global {
-	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface Platform {}
-	}
-}
+declare namespace App {
+	interface Locals { }
 
-export {};
+	interface Platform {
+		env: {
+			COUNTER: DurableObjectNamespace;
+		};
+		context: {
+			waitUntil(promise: Promise<any>): void;
+		};
+		caches: CacheStorage & { default: Cache }
+	}
+
+	interface Session { }
+
+	interface Stuff { }
+}
